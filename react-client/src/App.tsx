1a35c91de4a1a43a5c6587890aa8179e5872174e
traysonkelii <traysonkelii@gmail.com>
2025-04-03 11:06:40 -0700

    integrated API

--- react-client/src/App.tsx
+++ react-client/src/App.tsx
@@ -38,7 +38,7 @@ const AppContent = () => {
               mb: 4,
             }}
           >
-            Boba Finder
+            Boba Fetch
           </Typography>
         </Box>
       </Container>
---
+++ react-client/src/services/api.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { BobaShopsResponse, Office, SortOption } from "../types";
+
+const API_BASE_URL = "http://localhost:5000/api";
+
+export const fetchOffices = async (): Promise<Office[]> => {
+  const response = await axios.get(`${API_BASE_URL}/offices`);
+  return response.data;
+};
+
+export const fetchBobaShops = async (
+  location: string,
+  sortBy: SortOption,
+  offset: number = 0
+): Promise<BobaShopsResponse> => {
+  const response = await axios.get(`${API_BASE_URL}/boba-shops`, {
+    params: {
+      location,
+      sortBy,
+      offset,
+    },
+  });
+  return response.data;
+};
